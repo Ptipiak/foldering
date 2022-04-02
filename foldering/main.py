2ddf8256e07570702e1b62cd18569cc51135a1d6
raphael.beck-protoy <raphael.beck-protoy@consultant.bva-group.com>
2022-04-02 23:12:42 +0200

    Making a new dict duck type object

--- foldering/main.py
+++ foldering/main.py
@@ -3,6 +3,7 @@ import logging
 from pathlib import Path
 from foldering.reader import Reader
 from foldering.implementations.validators import JsonValidator
+from foldering.implementations.configuration import Configuration
 
 logger = logging.getLogger(__name__)
 files = ["schemas", "parameters", "a"]
@@ -14,12 +15,13 @@ def main():
     iterator_schemas = reader.open(schema_path)
     param_path = Path("./conf/parameters")
     iterator_params = reader.open(param_path)
-    settings = {}
+    settings = Configuration()
     validator = JsonValidator()
     for schema, _ in map(reader.read, iterator_schemas):
         validator.schema = schema
     for param, path in map(reader.read, iterator_params):
         validator.validate(param)
-        ancestor = ".".join(utils.ancestors(path, param_path))
-        settings = utils.aggregate(settings, ancestor, param)
+        ancestors = utils.subset(files, path.parts)
+        logger.debug(f"ancestors: {ancestors}")
+        settings[tuple(ancestors)] = param
     logger.info(settings)
